b7cb083b61a44288a3258066d4abafaa17e574d8
Масальский Фёдор <feodormas1947@gmail.com>
2026-03-15 23:04:40 +0300

    code changes

--- feature2.py
+++ feature2.py
@@ -1,21 +1,33 @@
 from random import sample
 import string
-d=string.digits
-game='y'
-while game=='y':
-    m=sample(d,4)
-    s=''
-    for i in range(4):
-        s=s+str(m[i])
-    cnt=1
-    n=input('Попробуй угадать четырехзначное число:')
-    nflag=1
-    for i in n:
-        if i not in d:
+def run():
+    digits=list(string.digits)
+    game='y'
+    while game=='y':
+        m=sample(digits,4)
+        s=''
+        for i in range(4):
+            s=s+str(m[i])
+        cnt=1
+        n=input('Попытайтесь угадать четырехзначное число:')
+        nflag=1
+        if len(n)!=4:
             nflag=0
-            break
-    if len(n)==4 and nflag==1:
-        while n!=s:
+        for i in n:
+            if i not in digits:
+                nflag=0
+                break
+        while nflag==0:
+            print('Это не четырёхзначное число!')
+            n=input('Попытайтесь угадать ЧЕТЫРЁХЗНАЧНОЕ ЧИСЛО:')
+            nflag=1
+            if len(n)!=4:
+                nflag=0
+            for i in n:
+                if i not in digits:
+                    nflag=0
+                    break
+        while n != s:
             cnt+=1
             bull=0
             cow=0
@@ -24,8 +36,28 @@ while game=='y':
                     bull+=1
                 elif s.count(n[i])==1:
                     cow+=1
-            print(n, ':', 'быки -', bull, '   коровы -', cow)
-            n=input('Попробуй угадать четырехзначное число:')
-        print('Поздравляю! Ты угадал число', *s, 'за', cnt, 'попыток!')
+            print(n, ': быки -', bull, '    коровы -', cow)
+            n=input('Попытайтесь угадать четырехзначное число:')
+            nflag=1
+            if len(n)!=4:
+                nflag=0
+            for i in n:
+                if i not in digits:
+                    nflag=0
+                    break
+            while nflag==0:
+                print('Это не четырёхзначное число!')
+                n=input('Попытайтесь угадать ЧЕТЫРЁХЗНАЧНОЕ ЧИСЛО:')
+                nflag=1
+                if len(n)!=4:
+                    nflag=0
+                for i in n:
+                    if i not in digits:
+                        nflag=0
+                        break
+        print('Поздравляю! Ты угадал число', s, 'за', cnt, 'попыток!')
         game=input('Сыграть ещё раз? y/n:')
-        
+        while game!='y' and game != 'n':
+            print('Некорректый ввод!')
+            game=input('Сыграть ещё раз? y/n:')
+run()
