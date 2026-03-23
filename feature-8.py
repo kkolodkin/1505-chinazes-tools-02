ba55f88a90433f5b0ed3acaee3f812bc4f4a7a46
6r38JJQu <valeria.v.rymar@gmail.com>
2026-03-23 10:14:11 +0300

    Добавление feature-8

---
+++ feature-8.py
@@ -0,0 +1,24 @@
+def feature_8():
+    s = input("Введите сообщение: ")
+    flag = True
+    while flag:
+        try:
+            n = int(input("Введите ключ(для шифровки числа больше 0, для дешифровки числа меньше нуля: "))
+            flag = False
+        except:
+            print('Ошибка ввода данных!')
+            
+    res = ""
+    for i in s:
+        if 'a' <= i <= 'z':
+            res += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
+        elif 'A' <= i <= 'Z':
+            res += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
+        elif 'а' <= i <= 'я':
+            res += chr((ord(i) - ord('а') + n) % 32 + ord('а'))
+        elif 'А' <= i <= 'Я':
+            res += chr((ord(i) - ord('А') + n) % 32 + ord('А'))
+        else:
+            res += i
+    print("Зашифрованное сообщение:", res)
+feature_8()
