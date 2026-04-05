796bfb0f40ebd8e8a8dd2c5542ec907b3ec2247b
VasilisaPI <va.piyakova@yandex.ru>
2026-04-05 20:50:01 +0300

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,9 +1,9 @@
 import requests
 
-def task_11():
+def run():
     print("\n--- Случайный совет ---")
     response = requests.get("https://api.adviceslip.com/advice")
     data = response.json()
     print(f"Совет: {data['slip']['advice']}")
 
-task_11()
+run()
