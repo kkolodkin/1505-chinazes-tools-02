04ad4dbfc2f13813a90ab403a4415fa36ffbfbdd
VasilisaPI <va.piyakova@yandex.ru>
2026-04-05 21:11:14 +0300

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,9 +1,9 @@
 import requests
 
-def task_10():
+def run():
     print("\n--- Случайная цитата ---")
     response = requests.get("https://api.quotable.io/random")
     data = response.json()
     print(f"\"{data['content']}\"\n— {data['author']}")
 
-task_10()
+run()
