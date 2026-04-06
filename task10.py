afb3cdbe2e70c2ec5bc3f70cd3c1edaeb19c2160
VasilisaPI <va.piyakova@yandex.ru>
2026-04-06 10:31:00 +0300

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,8 +1,12 @@
 import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 def run():
     print("\n--- Случайная цитата ---")
-    response = requests.get("https://api.quotable.io/random")
+    response = requests.get("https://api.quotable.io/random", verify=False)
     data = response.json()
     print(f"\"{data['content']}\"\n— {data['author']}")
 
