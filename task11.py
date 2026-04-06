beef54a3b657c159621f1c65b9a8f8d5e6c46c80
GYdra02 <feodormas1947@gmail.com>
2026-04-06 10:26:49 +0300

    rename and bugfix

--- task11.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def run():
-    print("\n--- Случайный совет ---")
-    response = requests.get("https://api.adviceslip.com/advice")
-    data = response.json()
-    print(f"Совет: {data['slip']['advice']}")
-
-run()
